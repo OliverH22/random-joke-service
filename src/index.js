bba1ed00fbd542afb2e097ab92bd294484b45965
Oliver <omh7431@rit.edi>
2021-02-10 15:42:20 -0500

    fixing broke code

--- src/index.js
+++ src/index.js
@@ -1,23 +1,19 @@
-
 /*
     Name: get-jokes.php
     Description: Returns an array of random jokes in JSON format
     Author: Oliver Hindman
-    Last Modified: 1/25/2021 
+    Last Modified: 1/25/2021
     Example usage: get-jokes.php?limit=3
 */
 
-
- let jokesq =["What do you call a very small valentine? A valen-tiny!","What did the dog say when he rubbed his tail on the sandpaper? Ruff, Ruff!","Why don't sharks like to eat clowns? Because they taste funny!","What did the boy cat say to the girl cat? You're Purr-fect!","What is a frog's favorite outdoor sport? Fly Fishing!","Did you hear about the cheese factory that exploded in France? Theyre the wurst.","Our wedding was so beautiful. Even the cake was in tiers.","Is this pool safe for diving? It deep ends.","Dad, can you put my shoes on? I dont think theyll fit me.",]
+const jokesq = ['What do you call a very small valentine? A valen-tiny!', 'What did the dog say when he rubbed his tail on the sandpaper? Ruff, Ruff!', "Why don't sharks like to eat clowns? Because they taste funny!", "What did the boy cat say to the girl cat? You're Purr-fect!", "What is a frog's favorite outdoor sport? Fly Fishing!", 'Did you hear about the cheese factory that exploded in France? Theyre the wurst.', 'Our wedding was so beautiful. Even the cake was in tiers.', 'Is this pool safe for diving? It deep ends.', 'Dad, can you put my shoes on? I dont think theyll fit me.'];
 
 const http = require('http');
 
-const url  = require('url');
+const url = require('url');
 const query = require('querystring');
 
-
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
-
 
 const indexPage = `
 <html>
@@ -33,7 +29,6 @@ const indexPage = `
   </body>
 </html>`;
 
-
 const errorPage = `
 <html>
   <head>
@@ -47,37 +42,37 @@ const errorPage = `
   </body>
 </html>`;
 
-const getRandomJokeJSON = (max=1) => {
-    const number = Math.floor(Math.random() * 11); 
-    const responseObj = {
-      timestamp: new Date(),
-      number: number
-    };
-    return JSON.stringify;
+const getRandomJokeJSON = (max = 1) => {
+  const number = Math.floor(Math.random() * 11);
+  const responseObj = {
+    timestamp: new Date(),
+    number,
   };
-
-const onRequest = (request, response) => {
-    //console.log(request.headers);
-    const parsedUrl = url.parse(request.url);
-    const pathname = parsedUrl.pathname;
-
-    console.log("parsedUrl=", parsedUrl);
-    console.log("pathname=", pathname);
-
-
-    if (pathname === "/") {
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.write(indexPage);
-        response.end();
-
-    } else {
-        response.writeHead(404, { 'Content-Type': 'text/html' });
-        response.write(errorPage);
-        response.end();
-    }
-
+  return JSON.stringify(responseObj);
 };
 
+const onRequest = (request, response) => {
+  // console.log(request.headers);
+  const parsedUrl = url.parse(request.url);
+  const { pathname } = parsedUrl;
+
+  console.log('parsedUrl=', parsedUrl);
+  console.log('pathname=', pathname);
+
+  if (pathname === '/') {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(indexPage);
+    response.end();
+  } else if (pathname === '/random-joke') {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.write(getRandomJokeJSON());
+    response.end();
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.write(errorPage);
+    response.end();
+  }
+};
 
 http.createServer(onRequest).listen(port);
 
